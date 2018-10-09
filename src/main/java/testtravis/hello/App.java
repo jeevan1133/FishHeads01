23dda7cd395fb024c007459bb5ddf5d406f5f53c
jeevan1133 <jeevan.basnet@selu.edu>
2018-10-08 21:40:01 -0700

    adding files

---
+++ src/main/java/testtravis/hello/App.java
@@ -0,0 +1,13 @@
+package testtravis.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Testing for SonarQube!!!" );
+    }
+}
